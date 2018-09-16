72896e46677a60d788beee160275dc75e5c17c89
Stefan Schneider <schneider@hrz.uni-marburg.de>
2018-09-16 16:56:00 +0200

    add sess parameter with ILIAS cookies for token.php

--- token.php
+++ token.php
@@ -12,15 +12,25 @@
  */ 
 chdir("../../../../../../../");
 
+if (empty($_COOKIE)) {
+    $_COOKIE = json_decode(base64_decode(rawurldecode($_GET['sess'])),TRUE);
+}
+
 // Avoid redirection to start screen
 // (see ilInitialisation::InitILIAS for details)
+
 $_GET["baseClass"] = "ilStartUpGUI";
 
 require_once "./include/inc.header.php";
 require_once "./Customizing/global/plugins/Services/Repository/RepositoryObject/XapiCmi5/classes/class.ilObjXapiCmi5.php";
 
 $track_obj = new ilObjXapiCmi5();
-echo CLIENT_ID . ':' . $track_obj->getToken();
+$token = base64_encode(CLIENT_ID . ':' . $track_obj->getToken());
+$res = array("auth-token" => $token);
 
-exit;
+//$token = base64_encode('isam:hdgtezdhztcghdzehdzusuejdzsuhtdh');
+//$res = array("auth-token" => $token);
+
+header('Content-type:application/json;charset=utf-8');
+echo json_encode($res);
 ?>
